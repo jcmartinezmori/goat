9d122becf11c935b0d996a5736c0da41abeccea9
Carlos Martinez <jcmartinezmori@Carloss-Air-2.lan>
2024-10-12 21:42:07 -0400

    .

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ if __name__ == '__main__':
     ver_l = ['nonadj']
     no_sam = 110000
     assc_l = ['atp', 'wta']
-    ctff_l = [3, 5, 10, 20]
+    ctff_l = [3, 5, 10]
 
     jobs = [(ver, assc, ctff, no_sam) for ver, assc, ctff in it.product(ver_l, assc_l, ctff_l)]
     jb.Parallel(n_jobs=16, verbose=11)(jb.delayed(main)(*job) for job in jobs)
