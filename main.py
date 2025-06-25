ccdcd5abe24436f67dba5b8d7e895c81433ad819
Carlos Martinez <jcmartinezmori@ipsec-10-2-226-174.vpn.gatech.edu>
2025-06-25 19:36:17 -0400

    .

--- main.py
+++ main.py
@@ -23,15 +23,10 @@ def main(version, association, cutoff, no_samples=100000):
 
 if __name__ == '__main__':
 
-    # ver_l = ['nonadj']
-    # no_sam = 100000
-    # assc_l = ['atp', 'wta']
-    # ctff_l = [3, 5, 10, 20]
-
     ver_l = ['nonadj']
-    no_sam = 2500000
+    no_sam = 10000000
     assc_l = ['atp', 'wta']
-    ctff_l = [3, 5]
+    ctff_l = [3, 5, 10, 20]
 
     jobs = [(ver, assc, ctff, no_sam) for ver, assc, ctff in it.product(ver_l, assc_l, ctff_l)]
     jb.Parallel(n_jobs=16, verbose=11)(jb.delayed(main)(*job) for job in jobs)
